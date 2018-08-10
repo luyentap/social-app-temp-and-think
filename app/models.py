1fdd3eda66a90c2615273e20f253378b7ae12bd2
luyentap <kevoruy@10vpn.info>
2018-08-10 11:07:25 +0000

    edit model

--- app/models.py
+++ app/models.py
@@ -1,20 +1,31 @@
 #luu vao file word cac buoc chi chi
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers  import make_password
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #profile
+        
+    birthday = models.DateField()
+    address = models.CharField(max_length=100)
+    
+    
     @property
     def full_name(self):
         "Returns the user's full name."
         return '%s %s' % (self.user.first_name, self.user.last_name)
-        
-    birthday = models.DateField()
-    address = models.CharField(max_length=100)
-
+    
+    #save user have encrypted password
+    def save(self, *args, **kwargs):
+        user = User.objects.get(username=self.user.username)
+        user.set_password(user.password);
+        user.save()
+    
+        super(Profile, self).save(*args, **kwargs)
+    
     def __str__(self):
         return str(self.full_name)
+
 
 class Friend(models.Model):
     """
@@ -25,13 +36,13 @@ class Friend(models.Model):
         
     """
     STATUS_FRIEND = (
-            (0,'reset_request'),
-            (1,'is_request'),
-            (2,'accept_friend'),
+            ('0','reset_request'),
+            ('1','is_request'),
+            ('2','accept_friend'),
     )
     
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE,related_name="username")
-    friend = models.ForeignKey(Profile, on_delete=models.CASCADE,related_name="friend")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="user")
+    friend = models.ForeignKey(User, on_delete=models.CASCADE,related_name="friend")
     status = models.CharField(max_length=1,choices=STATUS_FRIEND)
     """
     is a friend: check 
@@ -47,7 +58,7 @@ class Friend(models.Model):
 class Post(models.Model):
     content = models.TextField(max_length=2000)
     image = models.ImageField(upload_to='posts/%Y/%m/%d/')
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     #count of like in this  post
     count_like = models.IntegerField(default=0)
     #count of comment in this  post
@@ -59,7 +70,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
-    commenter = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.post)
@@ -67,7 +78,7 @@ class Comment(models.Model):
    
 class Like (models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    liker = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    liker = models.ForeignKey(User, on_delete=models.CASCADE)
    
     def __str__(self):
         return str(self.post)
